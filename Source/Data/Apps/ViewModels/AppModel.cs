c410dc0effcb2a06f73a56463c7fc759cbf1d148
brian <xan.brian@gmail.com>
2023-03-07 18:15:38 +0800

    up

--- Source/Data/Apps/ViewModels/AppModel.cs
+++ Source/Data/Apps/ViewModels/AppModel.cs
@@ -21,8 +21,8 @@ namespace Insight.MTP.Client.Data.Apps.ViewModels
             view.txtName.EditValue = item.name;
             view.txtAlias.EditValue = item.alias;
             view.txtHost.EditValue = item.domain;
-            view.txtTokenLife.EditValue = item.tokenLife / 1000;
-            view.txtPermitLife.EditValue = item.permitLife / 1000;
+            view.txtTokenLife.EditValue = item.tokenLife;
+            view.txtPermitLife.EditValue = item.permitLife;
             view.spiIndex.EditValue = item.index;
             view.cheSigninOne.Checked = item.signinOne;
             view.cheAutoRefresh.Checked = item.autoRefresh;
@@ -31,8 +31,8 @@ namespace Insight.MTP.Client.Data.Apps.ViewModels
             view.txtName.EditValueChanged += (sender, args) => item.name = view.txtName.Text.Trim();
             view.txtAlias.EditValueChanged += (sender, args) => item.alias = view.txtAlias.Text.Trim();
             view.txtHost.EditValueChanged += (sender, args) => item.domain = view.txtHost.Text.Trim();
-            view.txtPermitLife.EditValueChanged += (sender, args) => item.permitLife = 1000 * Convert.ToInt32(view.txtPermitLife.Text.Trim());
-            view.txtTokenLife.EditValueChanged += (sender, args) => item.tokenLife = 1000 * Convert.ToInt32(view.txtTokenLife.Text.Trim());
+            view.txtPermitLife.EditValueChanged += (sender, args) => item.permitLife = Convert.ToInt32(view.txtPermitLife.Text.Trim());
+            view.txtTokenLife.EditValueChanged += (sender, args) => item.tokenLife = Convert.ToInt32(view.txtTokenLife.Text.Trim());
             view.spiIndex.ValueChanged += (sender, args) => item.index = (int)view.spiIndex.Value;
             view.cheSigninOne.CheckedChanged += (sender, args) => item.signinOne = view.cheSigninOne.Checked;
             view.cheAutoRefresh.CheckedChanged += (sender, args) => item.autoRefresh = view.cheAutoRefresh.Checked;
